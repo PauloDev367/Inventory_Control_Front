10e6aad06128c1d874f5500d83a0593757388253
Paulo Silva <pauloecomercial367@gmail.com>
2025-04-17 08:43:57 -0300

    :sparkles: authentication guard created, and basic jwt auth verification configured

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { authGuard } from './account/shared/auth.guard';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AuthenticationComponent } from './layout/authentication/authentication.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    {
+        path: '',
+        component: AuthenticationComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent }
+        ],
+        canActivate: [authGuard]
+    }
 ];
